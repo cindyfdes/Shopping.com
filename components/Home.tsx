c34cf5fe8cfcb53d07cae84a5fcf38b078dd10a4
cindyfdes <cindyfernandes0308@gmail.com>
2022-06-14 23:52:01 +0530

    added key to div

--- components/Home.tsx
+++ components/Home.tsx
@@ -17,7 +17,7 @@ export const ItemsList = ({ products }: Props) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-6 gap-y-10 gap-x-6">
       {products.map((product) => (
-        <div>
+        <div key={product.id}>
           <Items product={product} key={product.id} />
         </div>
       ))}
